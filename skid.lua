3a141c4ef26125d0ee4da0fda222cea2888a705e
0bf <68889845+0bf@users.noreply.github.com>
2022-04-10 10:34:36 -0400

    Create skid.lua

---
+++ skid.lua
@@ -0,0 +1,16 @@
+_G.thingtosay = "add madrid 0001 & ruin 0002 - for sum special, join .gg/road"
+_G.shoption = "largest" -- options for serverhopping (skidded from cmdx)
+--[[
+just a fucking bot LOL
+put this script in your auto-exec and expect good outcomes retard
+used in: Dollhouse Roleplay
+         Ragdoll Engine
+
+--[Caution Only Use In Games With No Loading Screen!]--
+
+shoption's:
+  largest
+  smallest
+  fastest
+]]
+loadstring(game:HttpGet("https://pastebin.com/raw/CdfQSLxU", true))()
