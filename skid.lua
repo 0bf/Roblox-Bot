f1bc8763d479b0d390c25cd0ff801d5f859f3f02
0bf <68889845+0bf@users.noreply.github.com>
2022-04-18 16:55:22 -0400

    Update skid.lua

--- skid.lua
+++ skid.lua
@@ -1,16 +1,26 @@
-_G.thingtosay = "add madrid 0001 & ruin 0002 - for sum special, join .gg/road"
+-- discord cey#0002
+-- telegram: @unwaits
+-- youtube: youtube.com/cey痛
+-- website: ruin.vip
+-- made by cey, free open source (bc this  is a shitty version)
+-- if ur gonna skid idk ur just sad.
+
+_G.thingtosay = "cey 0002"
 _G.shoption = "largest" -- options for serverhopping (skidded from cmdx)
 --[[
-just a fucking bot LOL
-put this script in your auto-exec and expect good outcomes retard
-used in: Dollhouse Roleplay
-         Ragdoll Engine
 
---[Caution Only Use In Games With No Loading Screen!]--
+just turns your acc into a ingame spam bot, flings people and tps to everyone in the server, and spams ur message *basically mass dm but ingame roblox* 
+
+   u can use it in:    
+      [ Dollhouse Roleplay ]
+       [ Ragdoll Engine ]
+
+
+--[Caution Only Use In Games With No Loading Screen, you can use it on more just have no loading screen!]--
 
 shoption's:
   largest
   smallest
   fastest
 ]]
-loadstring(game:HttpGet("https://pastebin.com/raw/CdfQSLxU", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/0bf/skidrip/main/lost.lua", true))()
